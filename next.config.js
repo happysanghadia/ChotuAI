c381d8f83686f97f969924e38f5f4f59faa7cf3f
happysanghadia <happysanghadia@gmail.com>
2025-12-19 14:57:17 +0530

    Fix GitHub Pages basePath - add /ChotuAI basePath for correct asset paths

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/ChotuAI',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  // Uncomment and set basePath if your site is served from a subdirectory
-  // For example, if your GitHub Pages URL is: https://username.github.io/repo-name/
-  // basePath: '/ChotuAI',
 }
 
 module.exports = nextConfig
